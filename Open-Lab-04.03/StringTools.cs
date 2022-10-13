812326f0537eef7f773542a768201de770b19c67
Richardjurco <113421214+Richardjurco@users.noreply.github.com>
2022-10-13 08:33:22 +0200

    Update StringTools.cs

--- Open-Lab-04.03/StringTools.cs
+++ Open-Lab-04.03/StringTools.cs
@@ -6,9 +6,16 @@ namespace Open_Lab_04._03
     {
         public string RemoveFirstLast(string original)
         {
-            string result = original.Substring(1, original.Length - 2);
-            return result;
-        } if (string.original = )
+            if (original.Length > 2)
+            {
+                return original.Substring(1, original.Length - 2);
+            }
+            else if (original.Length <2 && original.Length > 0)
+            {
+                return original; 
+            }
+            else  return string.Empty;
+        } 
 
     }
 }
